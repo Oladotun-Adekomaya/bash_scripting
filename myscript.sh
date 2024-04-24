62df4001bf4ef4c7a73a80b15bc972f0611623f1
Oladotun Adekomaya <oladotunadekomaya@gmail.com>
2024-04-24 19:46:31 +0100

    =myscript.sh - stored the output of a command in a variable by making use of subshell

--- myscript.sh
+++ myscript.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-myname="Oladotun"
-age="40"
+# VARIABLES
 
-echo "Hello, my name is $myname."
-echo "I'm $age years old"
+#myname="Oladotun"
+#age="40"
+
+#echo "Hello, my name is $myname."
+#echo "I'm $age years old"
+
+#SUBSHELL - this is a way for you to save the output of a command to a variable the $(input your command here) is a subshell that allows 
+#you to run commands and save the result in a variable 
+
+files=$(pwd)
+echo $files
+
+
