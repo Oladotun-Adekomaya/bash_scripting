e589d880cfbe0f7573715cd506b63eebee93a209
Oladotun Adekomaya <oladotunadekomaya@gmail.com>
2024-04-25 12:03:00 +0100

    =myscript.sh - added some if statement in bash, wrote a code to check for files and directories in the file system, wrote some code to check if a particular program is present on the computer system, and if not, install it and run it

--- myscript.sh
+++ myscript.sh
@@ -25,9 +25,53 @@
 #$now
 #echo "Your username is: $USERNAME" #USERNAME is an environment/system variable
 
-#MATHS FUNCTION IN BASH
+# MATHS FUNCTION IN BASH
 
 #expr 10 + 20
 #expr 10 - 20
 #expr 10 \* 20
 #expr 20 / 10
+
+# IF STATEMENTS
+
+mynum=300
+
+#if [ $mynum -eq 200 ] # equal
+#then
+#    echo "The condition is true"
+#else
+#    echo "The variable does not equal 200"
+#fi
+
+#if [  $mynum -ne 200 ] #not equal
+#then
+#    echo "The condition is true"
+#else
+#    echo "The variable does not equal 200"
+#fi
+
+# -gt means greater than
+
+# HOW TO CHECK FOR FILES ON THE FILESYSTEM
+
+#if [ -f ~/Desktop/bash_scripting/myscript.sh ] # to check for directories, we change -f to -d
+#then
+#    echo "The file exists."
+#else
+#    echo "The file does not exist"
+#fi
+
+
+# A SIMPLE SCRIPT TO CHECK IF A PACKAGE HTOP IS INSTALLED AND INSTALL IT IF NOT FOUND. AND ALSO IMMEDIATELY RUN IT
+
+command=/usr/bin/htop
+
+if [ -f $command ]
+then
+    echo "$command is available, let's run it..."
+else
+    echo "$command is NOT available, installing it..."
+    sudo apt update && sudo apt install -y htop
+fi
+
+$command
