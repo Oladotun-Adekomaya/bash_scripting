6b6afaf20035ef0d9e936ca13bef59f19c3017f4
Oladotun Adekomaya <oladotunadekomaya@gmail.com>
2024-04-25 10:48:38 +0100

    =myscript.sh - added maths function to perform arithmetic operations in bash

--- myscript.sh
+++ myscript.sh
@@ -9,9 +9,25 @@
 #echo "I'm $age years old"
 
 #SUBSHELL - this is a way for you to save the output of a command to a variable the $(input your command here) is a subshell that allows 
-#you to run commands and save the result in a variable 
+#you to run commands and save the result in a variable
 
-files=$(pwd)
-echo $files
+#files=$(pwd)
+#echo $files
 
+#now=$(date)
+#echo "The system time and date is $now"
 
+#name="Oladotun Adekomaya"
+#now=$(date)
+
+#echo "Hello $name"
+#echo "The system time and date is"
+#$now
+#echo "Your username is: $USERNAME" #USERNAME is an environment/system variable
+
+#MATHS FUNCTION IN BASH
+
+#expr 10 + 20
+#expr 10 - 20
+#expr 10 \* 20
+#expr 20 / 10
